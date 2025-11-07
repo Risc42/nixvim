bb75e8d5098d1e7b5e617ad37c570855f5686dde
Risc42 <gabin.palix@unistra.fr>
2025-11-07 16:56:11 +0100

    Added friendly-snippets and some updates on keybinds

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -34,7 +34,7 @@
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
             keywordLength = 3;
           }
-          { name = "copilot"; }
+          #{ name = "copilot"; }
           {
             name = "path"; # file system paths
             keywordLength = 3;
@@ -43,7 +43,7 @@
             name = "luasnip"; # snippets
             keywordLength = 3;
           }
-          { name = "L3MON4D3/LuaSnip"; }
+          #{ name = "L3MON4D3/LuaSnip"; }
         ];
 
         window = {
@@ -57,9 +57,9 @@
 
         mapping = {
           "<C-Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
+          "<Down>" = "cmp.mapping.select_next_item()";
+          "<Up>" = "cmp.mapping.select_prev_item()";
+          "<Space>" = "cmp.mapping.abort()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
--- config/plugins/snippets/luasnip.nix
+++ config/plugins/snippets/luasnip.nix
@@ -1,9 +1,15 @@
-_: {
+{
   plugins.luasnip = {
     enable = true;
+    friendly-snippets.enable = true;
     settings = {
       enable_autosnippets = true;
       store_selection_keys = "<Tab>";
     };
   };
+  extraConfigLua = ''
+    require("luasnip").filetype_extend("c", { "cdoc" })
+    require("luasnip").filetype_extend("cpp", { "cppdoc" })
+    require("luasnip").filetype_extend("sh", { "shelldoc" })
+  '';
 }
