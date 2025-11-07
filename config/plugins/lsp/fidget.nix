56db1abfcb24a775a26f778881c38a25166443e8
Risc42 <gabin.palix@unistra.fr>
2025-11-07 20:42:22 +0100

    u

--- config/plugins/lsp/fidget.nix
+++ config/plugins/lsp/fidget.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       logger = {
-        level = "debug"; # "off", "error", "warn", "info", "debug", "trace"
+        level = "warn"; # "off", "error", "warn", "info", "debug", "trace"
         float_precision = 1.0e-2; # Limit the number of decimals displayed for floats
       };
       progress = {
