2e48c3ba0185060d3ab73f445636fd872a766a4f
Risc42 <gabin.palix@unistra.fr>
2025-11-07 20:05:05 +0100

    updates

--- config/plugins/lsp/clangd-extensions.nix
+++ config/plugins/lsp/clangd-extensions.nix
@@ -1,33 +1,5 @@
 {
   plugins.clangd-extensions = {
     enable = true;
-    settings = {
-      fallbackFlags = [
-        "-std=c23"
-        "-Wall"
-        "-Wextra"
-      ];
-      args = [
-        "--background-index"
-        "--clang-tidy"
-      ];
-    };
   };
-  extraConfigLua = ''
-    local clangd_ext = require("clangd_extensions")
-    clangd_ext.setup({
-        inlay_hints = {
-          auto = true,
-          show_parameter_hints = true,
-          parameter_hints_prefix = "<- ",
-          other_hints_prefix = "=> ",
-        },
-        ast = {
-          role_icons = {},
-          kind_icons = {},
-        },
-        memory_usage = { border = "none" },
-        symbol_info = { border = "none" },
-      })
-  '';
 }
