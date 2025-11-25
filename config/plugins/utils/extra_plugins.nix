f2ef5d8cd20909c9dbd30f456007fabc6c4dc6bd
Gabin Palix <gabin.palix@unistra.fr>
2025-11-25 23:47:57 +0100

    Coq-lsp-nvim

--- config/plugins/utils/extra_plugins.nix
+++ config/plugins/utils/extra_plugins.nix
@@ -1,15 +1,14 @@
 { pkgs, ... }:
-let
-  # The Rocq/Coq LSP client plugin
-  coqLspNvim = pkgs.vimPlugins.coq-lsp-nvim;
-in
+#let
+# The Rocq/Coq LSP client plugin
+#coqLspNvim = pkgs.vimPlugins.coq-lsp-nvim;
+#in
 {
   extraPackages = with pkgs; [
     coqPackages.coq-lsp # Rocq/Coq LSP server
     coq # Core Rocq/Coq executable
   ];
   extraPlugins = with pkgs.vimPlugins; [
-    #coqtail
-    coqLspNvim
+    coq-lsp-nvim
   ];
 }
