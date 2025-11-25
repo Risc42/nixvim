2b91becb42c1c15e2ec341483031e4b936e56294
Gabin Palix <gabin.palix@unistra.fr>
2025-11-25 23:58:03 +0100

    Fixed coq-nvim to get latest update

--- config/plugins/utils/extra_plugins.nix
+++ config/plugins/utils/extra_plugins.nix
@@ -9,6 +9,14 @@
     coq # Core Rocq/Coq executable
   ];
   extraPlugins = with pkgs.vimPlugins; [
-    coq-lsp-nvim
+    #coq-lsp-nvim
+    (pkgs.vimPlugins.coq-lsp-nvim.overrideAttrs (_: {
+      src = pkgs.fetchFromGitHub {
+        owner = "tomtomjhj";
+        repo = "coq-lsp.nvim";
+        rev = "master"; # or a pinned commit
+        sha256 = "sha256-1a45345nqr0hygagsqrqh0v1a1254qv10fh8rp3rkgqsqvj9sdcy=";
+      };
+    }))
   ];
 }
