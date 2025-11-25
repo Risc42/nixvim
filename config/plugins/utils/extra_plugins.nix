cd2da2072418c96765f61592098767e68893d62d
Gabin Palix <gabin.palix@unistra.fr>
2025-11-26 00:09:47 +0100

    Fixed coq-nvim to get latest update

--- config/plugins/utils/extra_plugins.nix
+++ config/plugins/utils/extra_plugins.nix
@@ -7,6 +7,9 @@
   extraPackages = with pkgs; [
     coqPackages.coq-lsp # Rocq/Coq LSP server
     coq # Core Rocq/Coq executable
+    coqPackages.mathcomp # Provides basic SSReflect/MathComp libraries
+    coqPackages.stdpp # Some standard Coq tactics (optional)
+    coqPackages.stdlib
   ];
   extraPlugins = with pkgs.vimPlugins; [
     coq-lsp-nvim
